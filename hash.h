5aca8efba1da3e169d24102a891b14dc8030598e
Le Nguyen <le.nguyenhoang2910@gmail.com>
2025-03-01 13:47:49 -0700

    Setups
    Basic setups and sample inputs

---
+++ hash.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+// You are free to use additional libraries as long as it's not PROHIBITED per instruction.
+
+using namespace std;
+class HashTable {
+
+    int hash_function(string text);
+}; 
