a9d3b0beade7214571e49c9cfe24856ac112819a
Le Nguyen <le.nguyenhoang2910@gmail.com>
2025-03-02 17:23:52 -0700

    Current hash.h
    Update more functions and attributes

--- hash.h
+++ hash.h
@@ -4,17 +4,63 @@
 // You are free to use additional libraries as long as it's not PROHIBITED per instruction.
 
 using namespace std;
-class HashTable {
 
-    int _slots;
+// Node class
+// class Node
+// {
+//     /* data */
+//     string _word;
+//     Node *_next;
 
-    public: 
-        // constructor 
-        HashTable(int slots);
-        // Deconstructor
-        ~HashTable();
+//     // Constructor
+//     Node(string word)
+//     {
+//         _word = word;
+//         _next = nullptr;
+//     }
+// };
+
+// Node class for linking item
+struct Node
+{
+    string _word;
+    Node *_next;
+
+    // Constructor
+    //  First element will kth slot, if k = 5
+    //  0  ->   nullptr
+    //  1  ->   nullptr
+    //  2  ->   nullptr
+    //  3  ->   nullptr
+    //  4  ->   nullptr
+    Node(string word)
+    {
+        _word = word;
+        _next = nullptr;
+    }
+};
+
+class HashTable
+{
+    Node **table; // pointer of pointer of element, this will be an array
+    int _slots;   // numbers of slot
+
+public:
+    // constructor
+    HashTable(int slots);
+    // Deconstructor
+    ~HashTable();
+
+    // Mutator
+    string prepend(string word);
+    string append(string word); // Doesn't work yet
+    double stdDev();
 
     int hash_function(string text);
-    void display();
+    void displayLength(); // Display the length of each slot
+    void display();       // Display each slot items
 
-}; 
+private:
+    double mean();     // Find the mean
+    double variance(); // Find the variance
+};
