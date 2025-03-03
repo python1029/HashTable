42827b0ec87bf879e283998f56ad9e55aa988dee
Le Nguyen <le.nguyenhoang2910@gmail.com>
2025-03-02 17:35:47 -0700

    Update hash.cpp
    Update functionalities of hash.h

--- hash.cpp
+++ hash.cpp
@@ -4,41 +4,159 @@
 
 using namespace std;
 
-struct Node {
-    string _word;
-    Node* _next;
-
-    //Constructor
-    // First element will kth slot, if k = 5 
-    // 0  ->   nullptr 
-    // 1  ->   nullptr 
-    // 2  ->   nullptr 
-    // 3  ->   nullptr 
-    // 4  ->   nullptr 
-    Node(string word) {
-        _word = word;
-        _next = nullptr; 
-    }
-};
-
-//Constructor 
-HashTable::HashTable(int slots){
+// Constructor
+HashTable::HashTable(int slots)
+{
     _slots = slots;
-    Node** table; // pointer of pointer of element 
-    table = new Node*[slots];
-    for (int i = 0; i < slots ; i++) {
-        table[i] = nullptr; // Right now all slots are empty 
-    } 
+    table = new Node *[slots]; // an dynamic array that stores pointers to linked list heads
+    for (int i = 0; i < slots; i++)
+    {
+        table[i] = nullptr; // Right now all slots are empty
+    }
 }
 
-HashTable::~HashTable() { 
-
+// Deconstructor
+HashTable::~HashTable()
+{
+    for (int i = 0; i < _slots; i++)
+    {
+        Node *currentNode = table[i];
+        while (currentNode)
+        {
+            Node *temp = currentNode;
+            currentNode = currentNode->_next;
+            delete temp;
+        }
+    }
+    delete[] table; // Deallocate the table from heap memory
 }
 
-int HashTable::hash_function(string text) {
+/// @brief Hash function, that finds value it in a slot
+/// @param text The string value
+/// @return Integer value
+int HashTable::hash_function(string text)
+{
     // Implement your own hash function here
-    int totalSlot = _slots; 
-    char letter = text[0]; // Take the first character(in ASCII) of the word 
-    int hashValue = letter % totalSlot; // Get the remainder 
+    int totalSlot = _slots;
+    char letter = text[0];              // Take the first character(in ASCII) of the word
+    int hashValue = letter % totalSlot; // Get the remainder
     return hashValue;
 }
+
+/// @brief Add new string at the head of a slot
+/// @param word The new string
+/// @return The added Node's value
+string HashTable::prepend(string word)
+{
+    // New Node pointer
+    Node *newNode = new Node(word);
+    int hashValue = hash_function(word);
+    newNode->_next = table[hashValue];
+    table[hashValue] = newNode;
+    return word;
+}
+
+/// @brief Display the current state of Hash Table
+void HashTable::display()
+{
+    for (int i = 0; i < _slots; i++)
+    {
+        cout << "Slot " << i << ": ";
+        Node *currentNode = table[i];
+        while (currentNode)
+        {
+            cout << " " << currentNode->_word;
+            currentNode = currentNode->_next;
+        }
+        cout << endl;
+    }
+}
+
+/// @brief Display the length of each slot of the Hash Table
+void HashTable::displayLength()
+{
+    for (int i = 0; i < _slots; i++)
+    {
+        int count = 0;
+        Node *currentNode = table[i];
+        while (currentNode)
+        {
+            currentNode = currentNode->_next;
+            count++;
+        }
+        cout << "Slot " << i << ": " << count << endl;
+    }
+}
+
+/// @brief Find the mean of the Hash Table by taking all (strings/slots)
+/// @return The mean
+double HashTable::mean()
+{
+    double mean = 0.0;
+    for (int i = 0; i < _slots; i++)
+    {
+        int count = 0;
+        Node *currentNode = table[i];
+        while (currentNode)
+        {
+            count++;
+            currentNode = currentNode->_next;
+        }
+        mean += count;
+    }
+    mean /= _slots;
+    return mean;
+}
+
+/// @brief Find the variance of Hash Table by taking ( sum( (Xs_of_a_slot - mean) ^ 2 ) ) / slots
+/// @return The variance
+double HashTable::variance()
+{
+    double variance = 0.0;
+    int *slotLengths = new int[_slots];
+    for (int i = 0; i < _slots; i++)
+    {
+        int count = 0;
+        Node *currentNode = table[i];
+        while (currentNode)
+        {
+            count++;
+            currentNode = currentNode->_next;
+        }
+        slotLengths[i] = count;
+    }
+    for (int i = 0; i < _slots; i++)
+    {
+        variance += pow(slotLengths[i] - mean(), 2);
+    }
+    variance /= _slots;
+    delete slotLengths;
+    return variance;
+}
+
+/// @brief Find the standard deviation of the Hash Table
+/// Smaller stdDev the better performance of Hash Table
+/// @return The stanard deviation
+double HashTable::stdDev()
+{
+    double _variance = variance(), stdDev = 0;
+    stdDev = sqrt(_variance);
+    return stdDev;
+}
+
+// NOTE: this not work yet
+
+// Add new string at the end
+// string HashTable::append(string word)
+// {
+//     Node *newNode = new Node(word);
+//     int hashValue = hash_function(word);
+//     Node *currentNode = table[hashValue];
+//     while (currentNode->_next != nullptr)
+//     {
+//         currentNode = currentNode->_next;
+//     }
+//     currentNode->_next = newNode;
+//     newNode->_next = nullptr;
+//     return newNode->_word;
+// }
