582a506878c1048f68a7802f9f6f7feeac33b8ed
Le Nguyen <le.nguyenhoang2910@gmail.com>
2025-03-05 13:22:26 -0700

    Update hash.h

--- hash.h
+++ hash.h
@@ -4,15 +4,42 @@
 // You are free to use additional libraries as long as it's not PROHIBITED per instruction.
 
 using namespace std;
-class HashTable {
+// Node class for linking item
+struct Node
+{
+    string _word;
+    Node *_next;
+ 
+    // Constructor
+    //  First element will kth slot, if k = 5
+    //  0  ->   nullptr
+    //  1  ->   nullptr
+    //  2  ->   nullptr
+    //  3  ->   nullptr
+    //  4  ->   nullptr
+    Node(string word)
+    {
+        _word = word;
+        _next = nullptr;
+    }
+};
 
-    int _slots;
+class HashTable
+{
+    Node **table; // pointer of pointer of element, this will be an array
+    int _slots;   // numbers of slot
 
-    public: 
-        // constructor 
-        HashTable(int slots);
-        // Deconstructor
-        ~HashTable();
+public:
+    // constructor
+    HashTable(int slots);
+    // Deconstructor
+    ~HashTable();
+
+    // Mutator
+    string prepend(string word);
+    string append(string word);
+    double stdDev();
+
 
     int hash_function(string text);
     void display();
