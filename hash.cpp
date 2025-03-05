1003d35934d264e234155bdab6355400bee0674d
Le Nguyen <le.nguyenhoang2910@gmail.com>
2025-03-05 13:28:27 -0700

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -4,41 +4,27 @@
 
 using namespace std;
 
-struct Node {
-    string _word;
-    Node* _next;
-
-    //Constructor
-    // First element will kth slot, if k = 5 
-    // 0  ->   nullptr 
-    // 1  ->   nullptr 
-    // 2  ->   nullptr 
-    // 3  ->   nullptr 
-    // 4  ->   nullptr 
-    Node(string word) {
-        _word = word;
-        _next = nullptr; 
-    }
-};
-
-//Constructor 
-HashTable::HashTable(int slots){
+// Constructor
+HashTable::HashTable(int slots)
+{
     _slots = slots;
-    Node** table; // pointer of pointer of element 
-    table = new Node*[slots];
-    for (int i = 0; i < slots ; i++) {
-        table[i] = nullptr; // Right now all slots are empty 
-    } 
+    Node **table; // pointer of pointer of element
+    table = new Node *[slots];
+    for (int i = 0; i < slots; i++)
+    {
+        table[i] = nullptr; // Right now all slots are empty
+    }
 }
 
-HashTable::~HashTable() { 
-
+HashTable::~HashTable()
+{
 }
 
-int HashTable::hash_function(string text) {
+int HashTable::hash_function(string text)
+{
     // Implement your own hash function here
-    int totalSlot = _slots; 
-    char letter = text[0]; // Take the first character(in ASCII) of the word 
-    int hashValue = letter % totalSlot; // Get the remainder 
+    int totalSlot = _slots;
+    char letter = text[0];              // Take the first character(in ASCII) of the word
+    int hashValue = letter % totalSlot; // Get the remainder
     return hashValue;
 }
